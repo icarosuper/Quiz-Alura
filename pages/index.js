62711352043bdbcfcd6685fda07266daebada43b
Ícaro Motta <icarosuperskate@gmail.com>
2021-01-25 16:00:04 -0300

    Projeto criado e em desenvolvimento

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,53 @@
 import styled from 'styled-components'
+import db from '../db.json'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+export const QuizBackground = styled.div`
+  background-image: url(${db.bg});
+`
+
+export const QuizLogo = styled.div`
+`
+
+export const Widget = styled.div`
+`
+
+export const Footer = styled.div`
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+	return (
+		<QuizBackground>
+			<QuizContainer>
+				<QuizLogo/>
+				<Widget>
+					<Widget.Header>
+						<h1>{db.title}</h1>
+					</Widget.Header>
+					<Widget.Content>
+						<p>{db.description}</p>
+					</Widget.Content>
+				</Widget>
+
+				<Widget>
+					<Widget.Content>
+						<h1>Quizes da Galera</h1>
+
+						<p>lorem ipsum dolor sit amet...</p>
+					</Widget.Content>
+				</Widget>
+				<Footer/>
+			</QuizContainer>
+		</QuizBackground>
+	);
 }
